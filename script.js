50754e3ac7af1ad130a27cb547e9330706e5d2b5
jcha0713 <joocha0713@gmail.com>
2020-01-30 17:05:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
+const text = document.querySelector(".floatingText")
 
 img1.addEventListener("mouseover", function() {
   img1.style.width = "350px";
   img1.style.height = "475px";
+  text.style.opacity = "0";
 });
 
 img2.addEventListener("mouseover", function() {
   img2.style.width = "350px";
   img2.style.height = "475px";
+  text.style.opacity = "0";
 });
 
 img1.addEventListener("mouseout", function() {
   img1.style.width = "50px";
   img1.style.height = "50px";
+  text.style.opacity = "1";
 });
 
 img2.addEventListener("mouseout", function() {
   img2.style.width = "50px";
   img2.style.height = "50px";
+  text.style.opacity = "1";
 });
